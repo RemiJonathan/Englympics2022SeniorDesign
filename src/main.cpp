aadad6406a7f3b1c5e02471ae49f94288b524dd7
Remi Jonathan Choquette <contact@remijonathan.com>
2022-11-06 12:15:35 -0500

    Setup Wifi Connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+String ssid = "Rémi Jonathan’s iPhone";
+String password = "ubuntuthecat";
+
 void setup() {
 // write your initialization code here
+//connect ESP8266 to Wi-Fi
+    Serial.begin(115200);
+    Serial.println("Connecting to WiFi");
+    WiFi.begin(ssid, password);
+//wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+//if connection fails, stop program
+        if (WiFi.status() == WL_CONNECT_FAILED) {
+            Serial.println("Connection failed");
+            break;
+        }
+    }
+    Serial.println("Connected to the WiFi network");
+
 }
 
 void loop() {
